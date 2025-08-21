7b3bd45367fdc997b6ab0bef614f9b370a46d61e
Wahlberg <allin.nikita@yandex.com>
2025-08-22 02:09:46 +0300

    add mesh prototype generation

--- Assets/VoronoiMapGen/Mesh/VoronoiMeshSystem.cs
+++ Assets/VoronoiMapGen/Mesh/VoronoiMeshSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Collections;
 using Unity.Entities;
+using Unity.Entities.Graphics;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
@@ -8,7 +9,6 @@ using UnityEngine;
 namespace VoronoiMapGen.Mesh
 {
     [UpdateInGroup(typeof(PresentationSystemGroup))]
-    [UpdateAfter(typeof(VoronoiMapGen.Systems.MapGenerationSystem))]
     public partial class VoronoiMeshSystem : SystemBase
     {
         private EntityQuery _cellQuery;
@@ -16,6 +16,7 @@ namespace VoronoiMapGen.Mesh
         private EntityQuery _siteQuery;
         private EntityQuery _mapGeneratedQuery;
         private bool meshesGenerated = false;
+        private static Material _defaultMaterial;
 
         protected override void OnCreate()
         {
@@ -30,7 +31,6 @@ namespace VoronoiMapGen.Mesh
             if (meshesGenerated || _mapGeneratedQuery.IsEmpty)
                 return;
 
-            // Ждем завершения генерации карты
             GenerateCellMeshes();
             meshesGenerated = true;
         }
@@ -39,17 +39,20 @@ namespace VoronoiMapGen.Mesh
         {
             Debug.Log("Generating Voronoi cell meshes...");
 
-            // Получаем все данные
             var cells = _cellQuery.ToComponentDataArray<Components.VoronoiCell>(Allocator.TempJob);
             var sites = _siteQuery.ToComponentDataArray<Components.VoronoiSite>(Allocator.TempJob);
             var edges = _edgeQuery.ToComponentDataArray<Components.VoronoiEdge>(Allocator.TempJob);
 
-            // Создаем меш для каждой ячейки
-            for (int i = 0; i < cells.Length; i++)
+            // Создаем один общий материал
+            if (_defaultMaterial == null)
+            {
+                _defaultMaterial = GetDefaultMaterial();
+            }
+
+            for (int i = 0; i < math.min(cells.Length, 50); i++)
             {
                 var cell = cells[i];
                 
-                // Находим сайт для этой ячейки
                 float2 sitePosition = new float2(0, 0);
                 for (int j = 0; j < sites.Length; j++)
                 {
@@ -60,7 +63,7 @@ namespace VoronoiMapGen.Mesh
                     }
                 }
 
-                CreateCellMeshEntity(cell, sitePosition, edges);
+                CreateCellMeshEntity(cell, sitePosition, edges, _defaultMaterial);
             }
             
             cells.Dispose();
@@ -68,12 +71,11 @@ namespace VoronoiMapGen.Mesh
             edges.Dispose();
         }
 
-        private void CreateCellMeshEntity(Components.VoronoiCell cell, float2 sitePosition, NativeArray<Components.VoronoiEdge> allEdges)
+        private void CreateCellMeshEntity(Components.VoronoiCell cell, float2 sitePosition, NativeArray<Components.VoronoiEdge> allEdges, Material material)
         {
-            // Создаем новую сущность для меша
             var meshEntity = EntityManager.CreateEntity();
+
             
-            // Добавляем компоненты трансформации
             EntityManager.AddComponentData(meshEntity, new LocalTransform
             {
                 Position = new float3(sitePosition.x, sitePosition.y, 0),
@@ -81,31 +83,28 @@ namespace VoronoiMapGen.Mesh
                 Scale = 1.0f
             });
 
-            // Добавляем тег меша
-            EntityManager.AddComponentData(meshEntity, new VoronoiMeshTag
-            {
-                CellIndex = cell.SiteIndex
-            });
-
-            // Создаем и добавляем меш через BlobAssetReference (новый способ)
+            Debug.Log(meshEntity);
+            
             var mesh = CreateCellMesh(cell, sitePosition, allEdges);
             if (mesh != null)
             {
-                // Создаем RenderMeshArray для нового API
-                var meshArray = new RenderMeshArray(new[] { mesh }, new[] { GetDefaultMaterial() });
-                
-                EntityManager.AddComponentData(meshEntity, new RenderMeshArrayComponent
-                {
-                    RenderMeshArray = meshArray,
-                    MaterialIndices = new int[] { 0 },
-                    MeshIndices = new int[] { 0 }
-                });
+                var renderMeshArray = new RenderMeshArray(new[] { material }, new[] { mesh });
+
+                RenderMeshUtility.AddComponents(
+                    meshEntity,
+                    EntityManager,
+                    new RenderMeshDescription
+                    {
+                        FilterSettings = RenderFilterSettings.Default
+                    },
+                    renderMeshArray,
+                    MaterialMeshInfo.FromRenderMeshArrayIndices(0, 0) 
+                );
             }
         }
 
         private UnityEngine.Mesh CreateCellMesh(Components.VoronoiCell cell, float2 sitePosition, NativeArray<Components.VoronoiEdge> allEdges)
         {
-            // Собираем ребра, принадлежащие этой ячейке
             var cellEdges = new NativeList<Components.VoronoiEdge>(32, Allocator.Temp);
             
             for (int i = 0; i < allEdges.Length; i++)
@@ -117,14 +116,12 @@ namespace VoronoiMapGen.Mesh
                 }
             }
 
-            // Если нет ребер, создаем простой квадрат
             if (cellEdges.Length == 0)
             {
                 cellEdges.Dispose();
                 return MeshGenerationUtility.CreateQuadMesh(1.0f, 1.0f);
             }
 
-            // Создаем меш из ребер (пока упрощенная версия)
             var mesh = MeshGenerationUtility.CreateQuadMesh(1.0f, 1.0f);
             
             cellEdges.Dispose();
@@ -133,28 +130,19 @@ namespace VoronoiMapGen.Mesh
 
         private Material GetDefaultMaterial()
         {
-            var shader = Shader.Find("Standard");
+            var shader = Shader.Find("Universal Render Pipeline/Lit");
             if (shader == null)
-            {
-                shader = Shader.Find("Universal Render Pipeline/Lit");
-            }
+                shader = Shader.Find("Standard");
             
-            var material = new Material(shader);
-            material.color = new Color(
-                UnityEngine.Random.Range(0.3f, 0.8f),
-                UnityEngine.Random.Range(0.3f, 0.8f),
-                UnityEngine.Random.Range(0.3f, 0.8f)
-            );
+            var material = new Material(shader)
+            {
+                color = new Color(
+                    UnityEngine.Random.Range(0.3f, 0.8f),
+                    UnityEngine.Random.Range(0.3f, 0.8f),
+                    UnityEngine.Random.Range(0.3f, 0.8f)
+                )
+            };
             return material;
         }
-    }
-
-    // Компонент для нового RenderMesh API
-    public struct RenderMeshArrayComponent : IComponentData
-    {
-        public RenderMeshArray RenderMeshArray;
-        
-        public int[] MaterialIndices;
-        public int[] MeshIndices;
     }
 }
